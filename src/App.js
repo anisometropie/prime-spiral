54e8a3f02c31913b1108431a4c2cb49f3b7082fa
Valentin Chanas <valentin.chanas@dcbrain.com>
2019-05-05 21:41:53 +0200

    move array to state

--- src/App.js
+++ src/App.js
@@ -3,24 +3,34 @@ import NumberSquare from "./NumberSquare";
 import { isPrime, nextPerfectSquare } from "./math";
 import "./style.css";
 
-const SIZE = 20000;
-const numbers = [...Array(SIZE).keys()].map(n => ({
-  value: n + 1,
-  isPrime: isPrime(n + 1)
-}));
+let SIZE = 10000;
+
 const origin = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
 
-function App() {
-  const numberSquares = numbers.map(number => (
-    <NumberSquare
-      key={number.value}
-      n={number.value}
-      isPrime={number.isPrime}
-      origin={origin}
-      size={5}
-    />
-  ));
-  return <div className="App">{numberSquares}</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      size: SIZE,
+      numbers: [...Array(SIZE).keys()].map(n => ({
+        value: n + 1,
+        isPrime: isPrime(n + 1)
+      }))
+    };
+  }
+
+  render() {
+    const numberSquares = this.state.numbers.map(number => (
+      <NumberSquare
+        key={number.value}
+        n={number.value}
+        isPrime={number.isPrime}
+        origin={origin}
+        size={5}
+      />
+    ));
+    return <div className="App">{numberSquares}</div>;
+  }
 }
 
 export default App;
